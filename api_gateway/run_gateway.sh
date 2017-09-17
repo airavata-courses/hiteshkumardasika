6ce88b9f6d8a3752533bfb6fa3dae14c0cb42eb2
hiteshkumardasika <hdasika@indiana.edu>
2017-09-17 19:00:26 -0400

    Adding -d tag for running the containers

--- api_gateway/run_gateway.sh
+++ api_gateway/run_gateway.sh
@@ -3,4 +3,4 @@ docker rm gateway_cont
 docker rmi gateway_img
 mvn clean install
 docker build -t gateway_img .
-docker run -i -p 8085 --net="host" --name gateway_cont  gateway_img
+docker run -d -p 8085 --net="host" --name gateway_cont  gateway_img
--- microservice_two/docker/run_mstwo.sh
+++ microservice_two/docker/run_mstwo.sh
@@ -2,4 +2,4 @@
 docker rm ms_two_cont
 docker rmi ms_2
 docker build -t ms_2 .
-docker run -i --net="host" -p 5000 --name ms_two_cont ms_2
+docker run -d --net="host" -p 5000 --name ms_two_cont ms_2
