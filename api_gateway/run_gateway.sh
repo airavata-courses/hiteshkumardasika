ef01edeb93be1a228a71acbcec1965997eaff260
hiteshkumardasika <hdasika@indiana.edu>
2017-09-17 18:51:19 -0400

    Changing docker run commands

--- api_gateway/run_gateway.sh
+++ api_gateway/run_gateway.sh
@@ -3,4 +3,4 @@ docker rm gateway_cont
 docker rmi gateway_img
 mvn clean install
 docker build -t gateway_img .
-docker run -it -p 8085 --net="host" --name gateway_cont  gateway_img
+docker run -i -p 8085 --net="host" --name gateway_cont  gateway_img
--- microserviceone/run_msone.sh
+++ microserviceone/run_msone.sh
@@ -3,4 +3,4 @@ docker rm ms_one_cont
 docker rmi ms_one
 mvn clean install
 docker build -t ms_one .
-docker run -it --net="host" -p 8082 --name ms_one_cont  ms_one
+docker run -i --net="host" -p 8082 --name ms_one_cont  ms_one
